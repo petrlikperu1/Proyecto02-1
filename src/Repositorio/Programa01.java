bd9d4e12dbacc040de66afb3700001214fe74dc1
Jair <jgutierrez8@autonoma.edu.pe>
2020-04-28 07:00:19 -0500

    Segundo avance del codigo
    Segundo avance del codigo

--- src/Repositorio/Programa01.java
+++ src/Repositorio/Programa01.java
@@ -4,6 +4,10 @@ public class Programa01
 {
    public static void main(String[]args)
    {
-       System.out.println("Hola mundo cruel!!!!!");
+       int n1=12,n2=90,mayor=0;
+       if(n1>n2)
+           System.out.println("El mayor es:"+n1);
+       else
+       System.out.println("El mayor es   :"+n2);
    }
 }
